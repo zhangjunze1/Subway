ca845e14708cdf05b71be5bb997b6c246ac7f0c8
zhangjunze1 <67810639+zhangjunze1@users.noreply.github.com>
2020-11-04 11:53:17 +0800

    Add files via upload

--- Mymain.java
+++ Mymain.java
@@ -7,38 +7,34 @@ import java.util.*;
 
 public class Mymain {
 
-    public static HashMap<String,station> map = new HashMap<>();         //方便查找站点信息
-    public static List<line> LineSet= new ArrayList<>();                       //方便查找路线
+    public static HashMap<String,station> map = new HashMap<>();//方便查找站点信息
+    public static List<line> LineSet= new ArrayList<>();//方便查找路线
 
-    public static void getSubway(String filename) {              //读入线路数据
+    public static void getSubway(String filename) {//读入线路数据
         try {
             InputStreamReader reader = new InputStreamReader(new FileInputStream(filename));
             BufferedReader br = new BufferedReader(reader);
-
             String read = "";
-            read = br.readLine();                                                   //线路数量
+            read = br.readLine();//线路数量
             int LineNum = 0;
             LineNum = Integer.parseInt(read);
 
             for (int i = 0; i <= LineNum; i++) {
-                line linex = new line();                                            //当前存的line
-
+                line linex = new line();//当前存的line
                 read = br.readLine();
                 if(read==null){
                     return;
                 }
-                String[] stations = read.split(" ");                          //读入的line对应的所有station************************
-                linex.setLineName(stations[0]);                                     //输入线路名
+                String[] stations = read.split(" ");//读入的line对应的所有station
+                linex.setLineName(stations[0]);//输入线路名
                 if (stations.length<=1){
                     System.out.print("本线路无可乘站");
                     return;
                 }
                 for (int j = 1; j < stations.length - 1; j++) {
-
-                    station station1 = new station();                               //当前line中的station
+                    station station1 = new station();//当前line中的station
                     station station2 = new station();
-
-                    if (map.containsKey(stations[j])) {                              //如果map中已经有该站点则把这个站点拿出来处理
+                    if (map.containsKey(stations[j])) {//如果map中已经有该站点则把这个站点拿出来处理
                         station1 = map.get(stations[j]);
                         map.remove(stations[j]);
                     } else {
@@ -53,34 +49,25 @@ public class Mymain {
                         station2.setStationName(stations[j + 1]);
                         station2.setVisited(false);
                     }
-
-                    if (!station1.getLine().contains(linex.getLineName())) {                  //如果当前站未加入line中，则在line中当前站名
+                    if (!station1.getLine().contains(linex.getLineName()))//如果当前站未加入line中，则在line中当前站名
                         station1.AddStationLine(linex.getLineName());
-                    }
-                    if (!station2.getLine().contains(linex.getLineName())) {                //如果当前站未加入line中，则在line中当前站名
+                    if (!station2.getLine().contains(linex.getLineName()))//如果当前站未加入line中，则在line中当前站名
                         station2.AddStationLine(linex.getLineName());
-                    }
-
-                    if (!station1.getLinkStations().contains(station2)) {
+                    if (!station1.getLinkStations().contains(station2))
                         station1.AddLinkStation(station2);
-                    }
-                    if (!station2.getLinkStations().contains(station1)) {
+                    if (!station2.getLinkStations().contains(station1))
                         station2.AddLinkStation(station1);
-                    }
 
                     station1.setPreStation(station1.getStationName());
                     station2.setPreStation(station2.getStationName());
-
                     map.put(stations[j], station1);
                     map.put(stations[j + 1], station2);
-
                     if (!linex.getStations().contains(station1.getStationName())) {
                         linex.stationAdd(station1.getStationName());
                     }
                     if (!linex.getStations().contains(station2.getStationName())) {
                         linex.stationAdd(station2.getStationName());
                     }
-
                 }
                 LineSet.add(linex);
             }
@@ -88,7 +75,6 @@ public class Mymain {
         }catch(Exception e){
             e.printStackTrace();
         }
-
     }
 
     public static void getStationOfLine(String name ){                             //根据线路名字查找该线路所有站点
@@ -117,35 +103,19 @@ public class Mymain {
             map.get(temp).setDistance(0);
         }
         station nowStation = new station();
+        Queue<String> queue = new LinkedList<>();//存放遍历过的站点
 
-        Queue<String> queue = new LinkedList<>();                   //存放遍历过的站点
-
-        if (!map.containsKey(start)){                               //判断起点站是否存在
-            System.out.print("起点站不存在");
-            return;
-        }
-        if (!map.containsKey(end)){
-            System.out.print("终点站不存在");                         //判断终点站是否存在
-            return;
-        }
-
-        if (start.equals(end)){
-            System.out.print("起点站与终点站相同 本站为"+end);
-            return;
-        }
         nowStation = map.get(start);
-
         queue.add(start);
         while(!queue.isEmpty()){
             String nowStationName = queue.poll();
             map.get(nowStationName).setVisited(true);
-
             if (nowStation.getStationName().equals(end)){
                 break;
             }
             for (station station1 :map.get(nowStationName).getLinkStations()){
-                if(!map.get(station1.getStationName()).isVisited()){                    //未访问过的临近站点
-                    map.get(station1.getStationName()).setPreStation(nowStationName);   //为preStation赋值
+                if(!map.get(station1.getStationName()).isVisited()){//未访问过的临近站点
+                    map.get(station1.getStationName()).setPreStation(nowStationName);//为preStation赋值
                     map.get(station1.getStationName()).setDistance(map.get(nowStationName).getDistance()+1);//临近站的距离为本站距离加1
                     queue.offer(station1.getStationName());
                 }
@@ -154,22 +124,32 @@ public class Mymain {
     }
 
     public static void PrintPath(String start,String end){
-
+        if (!map.containsKey(start)){//判断起点站是否存在
+            System.out.print("起点站不存在");
+            return;
+        }
+        if (!map.containsKey(end)){
+            System.out.print("终点站不存在");//判断终点站是否存在
+            return;
+        }
+        if (start.equals(end)){
+            System.out.print("起点站与终点站相同 本站为"+end);
+            return;
+        }
+        if(start.equals(end))
+            return ;
         List<String> path = new ArrayList<>();
         Stack<String> printline = new Stack<>();
         int numStation = 1;//第几站
         int cnt = 0;//换乘次数
-
         String str = end;
-
         while(!str.equals(start)){
             path.add(str);
             printline.push(str);
             str = map.get(str).getPreStation();
         }
-        path.add(str);              //把起点放进去
+        path.add(str);//把start放入path
         printline.push(str);
-
         for (int i=1;i<path.size()-1;i++){
             if (map.get(path.get(i)).getLine().size()==1){
                 continue;
@@ -203,7 +183,6 @@ public class Mymain {
             if (!temp1.equals(temp2))
                 map.get(path.get(i)).setIfchange(true);
         }//判断path中的换乘站
-
         System.out.println("共"+path.size()+"站");
         while(!printline.empty()){
             String printStation = printline.pop();
@@ -233,10 +212,7 @@ public class Mymain {
             System.out.print(printStation+" ");
             numStation++;
         }
-
-
     }
-
 
     public static void main(String[] ards){
 
@@ -248,7 +224,6 @@ public class Mymain {
         System.out.println("1.查询线路(线路包含的所有站)");
         System.out.println("2.查询路线");
         System.out.print("请输入序号(1/2):");
-
         Scanner sc = new Scanner(System.in);
         int input = sc.nextInt();
         if (input==1){
@@ -270,5 +245,4 @@ public class Mymain {
             System.out.println("请重新选择");
         return;
     }
-
 }
